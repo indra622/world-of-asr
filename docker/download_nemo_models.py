6691b6d6130edb7e8055dbc352a1f82c3c9f80ec
Hosung Park <indra622@gmail.com>
2023-10-05 05:24:30 +0000

    bug fix

--- docker/download_nemo_models.py
+++ docker/download_nemo_models.py
@@ -1,3 +1,6 @@
 import nemo.collections.asr as nemo_asr
 
 asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(model_name="nvidia/stt_en_fastconformer_transducer_xlarge")
+
+
+del asr_model
