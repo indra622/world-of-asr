596f17137c7fb7ca72d112d5e63621c06c97656b
Hosung Park <indra622@gmail.com>
2024-01-25 19:17:25 +0900

    Add devcontainer configuration and post-create script

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install OpenAI and Dotenv for Python
+# TODO: Check why this can't be done in requirements.txt
+pip install whisper-timestamped
+pip install librosa
+pip install docker==6.1.3
+pip install gradio==3.45.1
+pip install ffmpeg-python
+pip install auditok
+pip install lightning
+pip install torchaudio
+pip install einops
+pip install pyannote.audio
+# Install the OpenAI packages for Node.js
+# (Python related dependencies are covered in requirements.txt)
+# echo "Installing OpenAI For Node.js" 
