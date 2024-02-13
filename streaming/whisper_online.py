5f0436a3f285ddbce771c31fbbfd1991617601ce
Hosung Park <indra622@gmail.com>
2024-02-13 17:27:50 +0900

    소켓 스트리밍 클라이언트 코드 변경 및 유닛 테스트 코드 추가

--- streaming/whisper_online.py
+++ streaming/whisper_online.py
@@ -107,7 +107,7 @@ class FasterWhisperASR(ASRBase):
 
 
         # this worked fast and reliably on NVIDIA L40
-        model = WhisperModel(model_size_or_path, device="cuda", compute_type="float16", download_root=cache_dir)
+        #model = WhisperModel(model_size_or_path, device="cuda", compute_type="float16", download_root=cache_dir)
 
         # or run on GPU with INT8
         # tested: the transcripts were different, probably worse than with FP16, and it was slightly (appx 20%) slower
@@ -115,7 +115,7 @@ class FasterWhisperASR(ASRBase):
 
         # or run on CPU with INT8
         # tested: works, but slow, appx 10-times than cuda FP16
-#        model = WhisperModel(modelsize, device="cpu", compute_type="int8") #, download_root="faster-disk-cache-dir/")
+        model = WhisperModel(modelsize, device="cpu", compute_type="int8") #, download_root="faster-disk-cache-dir/")
         return model
 
     #def transcribe(self, audio, init_prompt="Ready."):
